d976e6bfe1b7a96f732445e09989f62a0539e3a1
Luiz <luiz.henrique31415@gmail.com>
2024-10-17 09:55:07 -0300

    save

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema.Contexts;
+using Sistema.Models;
+
+namespace Sistema.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SistemaContext _context;
+
+        public LoginController(SistemaContext context) 
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logar(string email, string senha)
+        {
+            var professor = _context.Professores.FirstOrDefault(p => p.Email == email && p.Senha == senha);
+
+            if (professor != null)
+            {
+                HttpContext.Session.SetInt32("ProfessorId", professor.ProfessorId);
+                HttpContext.Session.SetString("NomeProfessor", professor.Nome!);
+
+                return RedirectToAction("Index", "Professor");
+            }
+            TempData["Mensagem"] = "Email ou senha incorreto, tente novamente.";
+
+            return RedirectToAction("Index", "Login");
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ Controllers/ProfessorController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Contexts;
+using Sistema.Models;
+
+namespace Sistema.Controllers
+{
+    public class ProfessorController : Controller
+    {
+        private readonly SistemaContext _context;
+
+        public ProfessorController(SistemaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            int? professorId = HttpContext.Session.GetInt32("ProfessorId");
+
+            if (professorId == null)
+            {
+                return RedirectToAction("Index", "Login");
+
+            }
+
+            var professor = _context.Professores.Find(professorId);
+
+            var turmas = _context.Turmas.Where(t => t.ProfessorId == professorId).ToList();
+
+            ViewBag.NomeProfessor = professor!.Nome;
+
+            return View(turmas);
+        }
+
+        [HttpPost]
+        public IActionResult CadastrarTurma(string nomeTurma)
+        {
+            int? professorId = HttpContext.Session.GetInt32("ProfessorId");
+
+            var turma = new Turma
+            {
+                Nome = nomeTurma,
+                ProfessorId = professorId
+            };
+
+            _context.Turmas.Add(turma);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirTurma(int turmaId)
+        {
+            var turma = _context.Turmas.Include(t => t.Atividades).FirstOrDefault(t => t.TurmaId == turmaId);
+
+            if(turma!.Atividades.Any())
+            {
+                TempData["Mensagem"] = "Voce nao pode excluir uma turma com atividades cadastradas";
+
+                return RedirectToAction("Index");
+            }
+
+            _context.Turmas.Remove(turma);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
